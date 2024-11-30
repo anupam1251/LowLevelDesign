e03ae40ce2556eb27a71d6a2e15486f3301f5777
Anupam Chaudhary <ak47.anupam125@gmail.com>
2024-12-01 01:32:55 +0530

    Make the Vehicle class an Abstract class

--- LiskovSubstitutionPrinciple/Vehicle.h
+++ LiskovSubstitutionPrinciple/Vehicle.h
@@ -1,7 +1,7 @@
 #pragma once
 class Vehicle {
 public:
-	virtual size_t getNumberofVehicle() const ;
-	virtual bool hasEngine() const ;
+	virtual size_t getNumberofVehicle() const = 0;
+	virtual bool hasEngine() const = 0;
 	virtual ~Vehicle() = default;
 };
